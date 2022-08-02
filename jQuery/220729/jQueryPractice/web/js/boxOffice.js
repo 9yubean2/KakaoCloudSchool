dee5e79c9507d52a4bf09fc0baa3f7e0a077a71d
9yubean2 <mi701170@naver.com>
2022-08-02 11:26:25 +0900

    220802

--- jQuery/220729/jQueryPractice/web/js/boxOffice.js
+++ jQuery/220729/jQueryPractice/web/js/boxOffice.js
@@ -2,6 +2,7 @@ function getData(){
    //서버 프로그램을 호출해서 결과를 받아와서 화면 처리
    //비동기 방식으로 처리
    //Ajax를 이용해서 처리
+    $("tbody").empty();
    let targetDate = $("#searchDate").val().replace(/-/g,'');
 
    $.ajax({
@@ -13,7 +14,7 @@ function getData(){
       //timeout: 3000
    }).done(
        function(result){
-           
+           // console.log(result);
            // movieName = result['boxOfficeResult']['dailyBoxOfficeList'][0]['movieNm'];
            let list = result['boxOfficeResult']['dailyBoxOfficeList'];
 
@@ -33,17 +34,22 @@ function getData(){
                        for( i=0;i<3;i++){
                            //console.log(actorlist[i].peopleNm);
                            actors += actorlist[i].peopleNm;
-                           actors+= ',';
+                           actors+= ' ';
                        }
                        $.ajax({
+                           async:false,
                            type:"GET",
                            dataType:'json', //파싱 필요 없다
-                           // timeout: 3000,
                            url: 'https://dapi.kakao.com/v2/search/image',
                            data: { query : `${item['movieNm']}포스터`},
                            headers : {Authorization: 'KakaoAK aa16998fa801571a3a5f17d1ee323ab3'}
-                       }).done(
+
+
+
+                       })
+                           .done(
                            function(result){
+                               console.log(result);
                                img_url =  result.documents[0].image_url;
                                $("tbody")
                                    .append(`<tr><td>${item['rank']}</td><td><img src="${img_url}" width="150"/></td><td>${item['movieNm']}</td><td>${actors}</td><td>${item['openDt']}</td><td><input type="button" id="btnDelete" onclick="deleteMovie()" value="삭제"/></td></tr>`);
@@ -52,11 +58,10 @@ function getData(){
                            function() {
                                alert('Error');
                            }
-                       );
+                       )
                        // $("tbody")
                        //     .append(`<tr><td>${item['rank']}</td><td><img src="" width="50"/></td><td>${item['movieNm']}</td><td>${actors}</td><td>${item['openDt']}</td></tr>`);
                    }
-
 
                ).fail(
                    function() {
@@ -73,26 +78,26 @@ function getData(){
    );
 }
 
-function loadPoster(movieNm){
-    let img_url = '';
-    $.ajax({
-        type:"GET",
-        dataType:'json', //파싱 필요 없다
-        // timeout: 3000,
-        url: 'https://dapi.kakao.com/v2/search/image',
-        data: { query : `${movieNm}`},
-        headers : {Authorization: 'KakaoAK aa16998fa801571a3a5f17d1ee323ab3'}
-    }).done(
-        function(result){
-            return result.documents[0].image_url;
-        }
-    ).fail(
-        function() {
-            alert('Error');
-        }
-    );
-
-}
+// function loadPoster(movieNm){
+//     let img_url = '';
+//     $.ajax({
+//         type:"GET",
+//         dataType:'json', //파싱 필요 없다
+//         // timeout: 3000,
+//         url: 'https://dapi.kakao.com/v2/search/image',
+//         data: { query : `${movieNm}`},
+//         headers : {Authorization: 'KakaoAK aa16998fa801571a3a5f17d1ee323ab3'}
+//     }).done(
+//         function(result){
+//             return result.documents[0].image_url;
+//         }
+//     ).fail(
+//         function() {
+//             alert('Error');
+//         }
+//     );
+//
+// }
 $(function(){
     $("#MyTable").on("click", "#btnDelete", function() {
         $(this).closest("tr").remove();
